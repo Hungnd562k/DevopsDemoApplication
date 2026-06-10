f76c05b0b4a79fa31c256ee2b0874c910466ffaf
hungnd <hungnd@navisoft.com.vn>
2026-06-11 00:12:05 +0700

    Init repository

---
+++ EnvVarController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1;
+
+[Route("api/env-vars")]
+public class EnvVarController : Controller
+{
+    [HttpGet("env-vars")]
+    public IActionResult Get()
+    {
+        return Ok();
+    }
+}
